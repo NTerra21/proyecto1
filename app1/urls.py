1289ae93cfb9a3bf8751fc0231c14bd3c49e4d32
Martín <martinmiranda1509@gmail.com>
2022-08-24 19:42:48 -0300

    .

--- app1/urls.py
+++ app1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from app1.views import  profesor, alumno
+from app1.views import  profesor,inicio, estudiantes
 
 
 urlpatterns = [
     path("profesor/", profesor, name='app1Profesor'),
-    path("", alumno, name= 'app1Inicio'),
+    path("estudiantes/", estudiantes, name='app1Estudiantes'),
+    path("", inicio, name= 'app1Inicio'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,21 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from app1.forms import AlumnoFormulario
+from app1.models import Estudiantes
+
+
+def estudiantes(request):
+
+    if request.method == 'POST':
+
+        miFormulario = AlumnoFormulario(request.POST)
+
+        print(miFormulario)
+
+        if miFormulario.is_valid:
+
+            informacion = miFormulario.cleaned_data
+
+            estudiante = Estudiantes(nombre=informacion['nombre'], apellido=informacion['apellido'],email=informacion['email'])
+
+            estudiante.save()
+
+            return render(request, "estudiantes.html")
+
+    else:
+
+        miFormulario = AlumnoFormulario()
+
+    return render(request, "estudiantes.html", {"miFormulario": miFormulario})
+
+
+def inicio(request):
+    return render(request, "inicio.html")
+
 
 def profesor(request):
-
-
-    prof = {
-        "profe1": {
-            "profe0":"profe",
-            "profe2": "profe2"}
-    }
-
-    plantilla = loader.get_template("Profesor.html")
-    documento_texto = plantilla.render(prof)
-
-    return HttpResponse(documento_texto)
-
-
-def alumno(request):
-    return render(request, "index.html")
+    return render(request, 'profesores.html')
